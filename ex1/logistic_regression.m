afc88f815b2e6c027711b54d77c3e22e0d6784f3
Aldian Fazrihady <mobile@aldian.net>
2015-11-24 20:05:14 +0700

    Logistic regression using loop.

--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -22,3 +22,9 @@ function [f,g] = logistic_regression(theta, X,y)
   %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
   %
 %%% YOUR CODE HERE %%%
+for i=1:m
+    x = X(:, i);
+    h = sigmoid(theta' * x);
+    f = f - (y(i) * log(h) + (1 - y(i)) * log(1 - h));
+    g = g + x * (theta' * x - y(i));
+end
